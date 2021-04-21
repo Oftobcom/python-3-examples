1c76924f7e3363e9cb42b39158f9768a10298bc5
OFTOB.COM <oftob.com@gmail.com>
2021-04-21 14:42:00 +0500

    slice.py 2021-04-21 14:41
    slice.py 2021-04-21 14:41

--- slice.py
+++ slice.py
@@ -1,15 +1,32 @@
 py_string = 'Python'
+print(py_string)
 
-slice_object = slice(3)
+slice_object = slice(1)
+print(py_string[slice_object])
+
+slice_object = slice(-1)
 print(py_string[slice_object])
 
 slice_object = slice(1, 6, 2)
 print(py_string[slice_object])   
 
-py_string='matematica'
+slice_object = slice(0, 6, 2)
+print(py_string[slice_object])   
 
-slice_object=slice(-2)
+
+print()
+
+py_string='matematica'
+print(py_string)
+
+slice_object=slice(3)
+print(py_string[slice_object])
+
+slice_object=slice(-3)
 print(py_string[slice_object])
 
 slice_object = slice(0,10,4)
+print(py_string[slice_object])
+
+slice_object = slice(1,len(py_string),4)
 print(py_string[slice_object])
--- slice2.py
+++ slice2.py
@@ -2,8 +2,10 @@ a="pyhton"
 print(a[1:len(a):2])
 print(a[:4])
 print(a[::2])
+print(a[::-1])
 
 b=(1,2,3,4,5,6,7,8,9,10)
 print(b[1:len(b):3])
 print(b[:4])
 print(b[::3])
+print(b[::-1])
