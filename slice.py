4efda3c546e932e3f02a584761f54d0035751d08
OFTOB.COM <oftob.com@gmail.com>
2021-04-21 14:26:36 +0500

    slice2.py 2021-04-21
    slice2.py 2021-04-21

--- slice.py
+++ slice.py
@@ -1,6 +1,15 @@
 py_string = 'Python'
+
 slice_object = slice(3)
 print(py_string[slice_object])
 
 slice_object = slice(1, 6, 2)
 print(py_string[slice_object])   
+
+py_string='matematica'
+
+slice_object=slice(-2)
+print(py_string[slice_object])
+
+slice_object = slice(0,10,4)
+print(py_string[slice_object])
